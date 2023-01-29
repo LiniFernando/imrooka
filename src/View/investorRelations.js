b1143fb56e47249c63c25e86a31874cf9397eb25
Dilini Fernando <linifernando123@gmail.com>
2023-01-29 18:04:11 +0530

    improve Investor Relations

--- src/View/investorRelations.js
+++ src/View/investorRelations.js
@@ -3,9 +3,54 @@ import homeImage from "../img/investors/slide.png";
 import about from "../img/home/welcome tile.png";
 import report from "../img/investors/report1.jpg";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {Grid, Container, Typography, Accordion, AccordionSummary, AccordionDetails} from "@mui/material";
+import {Grid, Container, Accordion, AccordionSummary, AccordionDetails} from "@mui/material";
+import {COLORS as Color} from "../Common/Constant/Color";
 
 const InvestorRelations = () => {
+    const reports = [
+        {
+            url: report,
+            title: 'brand1',
+            width: '40%',
+        },
+        {
+            url: report,
+            title: 'brand2',
+            width: '30%',
+        },
+        {
+            url: report,
+            title: 'brand3',
+            width: '30%',
+        },
+        {
+            url: report,
+            title: 'brand4',
+            width: '30%',
+        },
+    ];
+    const QandA = [
+        {
+            question: 'When will BIM release its quarterly results?',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendis semalesuada lacus ex, sit amet blandit leo lobortis eget.',
+        },
+        {
+            question: 'When will BIM release its quarterly results?',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendis semalesuada lacus ex, sit amet blandit leo lobortis eget.',
+        },
+        {
+            question: 'When will BIM release its quarterly results?',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendis semalesuada lacus ex, sit amet blandit leo lobortis eget.',
+        },
+        {
+            question: 'When will BIM release its quarterly results?',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendis semalesuada lacus ex, sit amet blandit leo lobortis eget.',
+        },
+        {
+            question: 'When will BIM release its quarterly results?',
+            answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendis semalesuada lacus ex, sit amet blandit leo lobortis eget.',
+        },
+    ];
     return (
         <div style={{
             backgroundColor: 'white', fontSize: ' calc(10px + 2vmin)',
@@ -13,8 +58,8 @@ const InvestorRelations = () => {
         }}>
             <img style={{width: "100%"}} src={homeImage} alt=""/>
             <div style={{textAlign: 'center'}}>
-                <h1 style={{color: '#10728D'}}>CORPORATE INFORMATION</h1>
-                <h2>The World of Bee. A journey spanning over 100 years</h2>
+                <h2 style={{color: Color.theme}}>CORPORATE INFORMATION</h2>
+                <h3>The World of Bee. A journey spanning over 100 years</h3>
             </div>
             {/*start information*/}
             <Grid container
@@ -24,23 +69,22 @@ const InvestorRelations = () => {
                 <Grid item xs={10} sm={5}>
                     <img style={{width: "100%"}} src={about} alt=""/>
                 </Grid>
-                <Grid item xs={10} sm={5} style={{textAlign: 'left'}}>
+                <Grid item xs={10} sm={5} style={{textAlign: 'left', marginLeft: 20}}>
+
+                    <h3 style={{color: Color.theme, margin: 0}}>Name</h3>
+                    <h3 style={{fontSize: 20, margin: 0}}>BEE IN MIND</h3>
                     <Container style={{padding: '20px 0'}}>
-                        <Typography style={{color: '#10728D'}}>Name</Typography>
-<Typography style={{fontSize: 20}}>BEE IN MIND</Typography>
+                        <h3 style={{color: Color.theme, margin: 0}}>Registration Number</h3>
+                        <h3 style={{fontSize: 20, margin: 0}}>AB 1234 FG/HJ</h3>
                     </Container>
                     <Container style={{padding: '20px 0'}}>
-                        <Typography style={{color: '#10728D'}}>Registration Number</Typography>
-                        <Typography style={{fontSize: 20}}>AB 1234 FG/HJ</Typography>
+                        <h3 style={{color: Color.theme, margin: 0}}>Registration Address</h3>
+                        <h3 style={{fontSize: 20, margin: 0}}>BLACK BRIGHT, ORUTOTA RD, GAMPAHA, SRI
+                            LANKA</h3>
                     </Container>
                     <Container style={{padding: '20px 0'}}>
-                        <Typography style={{color: '#10728D'}}>Registration Address</Typography>
-                        <Typography style={{fontSize: 20}}>BLACK BRIGHT, ORUTOTA RD, GAMPAHA, SRI
-                            LANKA</Typography>
-                    </Container>
-                    <Container style={{padding: '20px 0'}}>
-                        <Typography style={{color: '#10728D'}}>Date of incorporation</Typography>
-                        <Typography style={{fontSize: 20}}>09TH OF APRIL 1988</Typography>
+                        <h3 style={{color: Color.theme, margin: 0}}>Date of incorporation</h3>
+                        <h3 style={{fontSize: 20, margin: 0}}>09TH OF APRIL 1988</h3>
                     </Container>
                 </Grid>
             </Grid>
@@ -51,7 +95,7 @@ const InvestorRelations = () => {
                   direction="row"
                   justifyContent="center"
                   alignItems="center" spacing={1}
-                  style={{backgroundColor: '#C6ECE5', paddingBottom: '20px', marginTop: '50px',}}>
+                  style={{backgroundColor: Color.light, paddingBottom: '20px', marginTop: '50px',}}>
                 <Grid style={{paddingBottom: '20px', marginTop: '50px',}}>
                     <Grid
                         container
@@ -61,7 +105,7 @@ const InvestorRelations = () => {
                         spacing={5}
                     >
                         <Grid item>
-                            <h1 style={{color: '#10728D'}}>FINANCIAL REPORTS</h1>
+                            <h2 style={{color: Color.theme}}>FINANCIAL REPORTS</h2>
                         </Grid>
                     </Grid>
                     <Container style={{
@@ -74,18 +118,12 @@ const InvestorRelations = () => {
                             alignItems="center"
                             spacing={3}
                         >
-                            <Grid item xs={12} sm={3}>
-                                <img style={{width: "90%"}} src={report} alt=""/>
-                            </Grid>
-                            <Grid item xs={12} sm={3}>
-                                <img style={{width: "90%"}} src={report} alt=""/>
-                            </Grid>
-                            <Grid item xs={12} sm={3}>
-                                <img style={{width: "90%"}} src={report} alt=""/>
-                            </Grid>
-                            <Grid item xs={12} sm={3}>
-                                <img style={{width: "90%"}} src={report} alt=""/>
-                            </Grid>
+                            {reports.map((image) => (
+                                <Grid item xs={12} sm={3}>
+                                    <img style={{width: "70%"}} src={image.url} alt=""/>
+                                </Grid>
+
+                            ))}
                         </Grid>
                     </Container>
                 </Grid>
@@ -93,24 +131,24 @@ const InvestorRelations = () => {
             {/*end reports*/}
 
             {/*start investor contact*/}
-            <h1 style={{color: '#10728D'}}>INVESTOR CONTACTS</h1>
+            <h2 style={{color: Color.theme}}>INVESTOR CONTACTS</h2>
             <Grid container
                   direction="row"
                   justifyContent="center"
                   alignItems="center" spacing={1} style={{marginTop: "20px"}}>
                 <Grid item xs={10} sm={5}>
-                    <Typography style={{color: '#10728D', fontSize: 20}}>Lakshan Kumara</Typography>
-                    <Typography style={{fontSize: 20}}>Head, Strategic Business Development
-                        Unit</Typography>
-                    <Typography style={{fontSize: 20}}>asdfhb@bee.com</Typography>
-                    <Typography style={{fontSize: 20}}>+94 112 123 123</Typography>
+                    <h3 style={{color: Color.theme, fontSize: 20}}>Lakshan Kumara</h3>
+                    <h3 style={{fontSize: 20}}>Head, Strategic Business Development
+                        Unit</h3>
+                    <h3 style={{fontSize: 20}}>asdfhb@bee.com</h3>
+                    <h3 style={{fontSize: 20}}>+94 112 123 123</h3>
                 </Grid>
                 <Grid item xs={10} sm={5}>
-                    <Typography style={{color: '#10728D', fontSize: 20}}>Lakshan Kumara</Typography>
-                    <Typography style={{fontSize: 20}}>Head, Strategic Business Development
-                        Unit</Typography>
-                    <Typography style={{fontSize: 20}}>asdfhb@bee.com</Typography>
-                    <Typography style={{fontSize: 20}}>+94 112 123 123</Typography>
+                    <h3 style={{color: Color.theme, fontSize: 20}}>Lakshan Kumara</h3>
+                    <h3 style={{fontSize: 20}}>Head, Strategic Business Development
+                        Unit</h3>
+                    <h3 style={{fontSize: 20}}>asdfhb@bee.com</h3>
+                    <h3 style={{fontSize: 20}}>+94 112 123 123</h3>
                 </Grid>
             </Grid>
             {/*end investor contact*/}
@@ -119,7 +157,7 @@ const InvestorRelations = () => {
             <Grid container
                   justifyContent="center"
                   alignItems="center" spacing={1}
-                  style={{backgroundColor: '#C6ECE5', paddingBottom: '20px', marginTop: '50px',}}>
+                  style={{backgroundColor: Color.light, paddingBottom: '20px', marginTop: '50px',}}>
                 <Grid style={{paddingBottom: '20px', marginTop: '50px',}}>
                     <Grid
                         container
@@ -129,102 +167,27 @@ const InvestorRelations = () => {
                         spacing={5}
                     >
                         <Grid item>
-                            <h1 style={{color: '#10728D'}}>INVESTOR FAQS</h1>
+                            <h2 style={{color: Color.theme}}>INVESTOR FAQS</h2>
                         </Grid>
                     </Grid>
                     <Container style={{
                         marginTop: 20, marginBottom: 20, width: '80%',
                     }}>
-                        <Accordion style={{backgroundColor: '#8DD9CC', borderRadius: 10, margin: '5px 0'}}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon sx={{fontSize: 50}}/>}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography>When will BIM release its quarterly
-                                    results?</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                </Typography>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion style={{backgroundColor: '#8DD9CC', borderRadius: 10, margin: '5px 0'}}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon sx={{fontSize: 50}}/>}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography>When will BIM release its quarterly
-                                    results?</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                </Typography>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion style={{backgroundColor: '#8DD9CC', borderRadius: 10, margin: '5px 0'}}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon sx={{fontSize: 50}}/>}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography>When will BIM release its quarterly
-                                    results?</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                </Typography>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion style={{backgroundColor: '#8DD9CC', borderRadius: 10, margin: '5px 0'}}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon sx={{fontSize: 50}}/>}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography>When will BIM release its quarterly
-                                    results?</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                </Typography>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Accordion style={{backgroundColor: '#8DD9CC', borderRadius: 10, margin: '5px 0'}}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon sx={{fontSize: 50}}/>}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography>When will BIM release its quarterly
-                                    results?</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
-                                </Typography>
-                            </AccordionDetails>
-                        </Accordion>
+                        {QandA.map((qa) => (
+                            <Accordion style={{backgroundColor: Color.regular, borderRadius: 10, margin: '5px 0'}}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon sx={{fontSize: 50}}/>}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <h4 style={{margin: 0}}>{qa.question}</h4>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <h4 style={{margin: 0}}>
+                                        {qa.answer}</h4>
+                                </AccordionDetails>
+                            </Accordion>
+                        ))}
                     </Container>
                 </Grid>
             </Grid>
