a028f58ccd12aaf62b73bf5eb7297bd0ae4e6f63
Dilini Fernando <linifernando123@gmail.com>
2023-01-29 12:16:39 +0530

    Create Color Constant

---
+++ src/Common/Constant/Color.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+    theme: '#10728D',
+    regular: '#8DDACC',
+    light: '#C6ECE5',
+    white: '#fff',
+    black: '#000',
+    gradient: ['#10728D', '#8DDACC', '#C6ECE5'],
+};
