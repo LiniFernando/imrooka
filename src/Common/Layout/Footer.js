5563f3c9e4de86c52a4b70b67dbe5bcba9102cbb
Dilini Fernando <linifernando123@gmail.com>
2023-02-26 20:07:53 +0530

    add real address & contact to the footer

--- src/Common/Layout/Footer.js
+++ src/Common/Layout/Footer.js
@@ -30,23 +30,23 @@ const Footer = () => {
                               direction="row"
                               justifyContent="center"
                               alignItems="center" style={{width: '80%', marginTop: '20px'}}>
-                            <LinkedInIcon sx={{color: Color.white, fontSize: 40,margin:'0 5px'}}/>
-                            <TwitterIcon sx={{color: Color.white, fontSize: 40,margin:'0 5px'}}/>
-                            <FacebookIcon sx={{color: Color.white, fontSize: 40,margin:'0 5px'}}/>
-                            <InstagramIcon sx={{color: Color.white, fontSize: 40,margin:'0 5px'}}/>
-                            <YouTubeIcon sx={{color: Color.white, fontSize: 40,margin:'0 5px'}}/>
+                            <LinkedInIcon sx={{color: Color.white, fontSize: 40, margin: '0 5px'}}/>
+                            <TwitterIcon sx={{color: Color.white, fontSize: 40, margin: '0 5px'}}/>
+                            <FacebookIcon sx={{color: Color.white, fontSize: 40, margin: '0 5px'}}/>
+                            <InstagramIcon sx={{color: Color.white, fontSize: 40, margin: '0 5px'}}/>
+                            <YouTubeIcon sx={{color: Color.white, fontSize: 40, margin: '0 5px'}}/>
                         </Grid>
                     </Grid>
                     <Grid item xs={12} sm={2} style={{textAlign: 'left'}}>
                         <h2 style={{color: Color.white}}>QUICK LINKS</h2>
                         <Container
                             style={{display: 'flex', flexDirection: 'column', color: Color.regular, fontSize: 20}}>
-                            <Link href="" underline="none" color="inherit">Home</Link>
+                            <Link href="/" underline="none" color="inherit">Home</Link>
                             <Link href="/about" underline="none" color="inherit">About Us</Link>
                             <Link href="/sectors" underline="none" color="inherit">Sectors</Link>
-                            <Link href="/investorRelations" underline="none" color="inherit">Investor Relations</Link>
-                            <Link href="/careers" underline="none" color="inherit">Careers</Link>
-                            <Link href="/news" underline="none" color="inherit">News</Link>
+                            {/*<Link href="/investorRelations" underline="none" color="inherit">Investor Relations</Link>*/}
+                            <Link href="/career" underline="none" color="inherit">Careers</Link>
+                            {/*<Link href="/news" underline="none" color="inherit">News</Link>*/}
                             <Link href="/contact" underline="none" color="inherit">Contact Us</Link>
                         </Container>
                     </Grid>
@@ -55,32 +55,43 @@ const Footer = () => {
                         <h2 style={{color: Color.white}}>OUR SECTORS</h2>
                         <Container
                             style={{display: 'flex', flexDirection: 'column', color: Color.regular, fontSize: 20}}>
-                            <Link href="" underline="none" color="inherit">Technology &
+                            <Link href="/sectors/Ayurvedic & Beauty" underline="none" color="inherit">Ayurvedic &
+                                Beauty</Link>
+                            <Link href="/sectors/Products" underline="none" color="inherit">Products</Link>
+                            <Link href="/sectors/Technology & Research" underline="none" color="inherit">Technology &
                                 Research</Link>
-                            <Link href="" underline="none" color="inherit">Apparel</Link>
-                            <Link href="" underline="none" color="inherit">Clinic & Saloon</Link>
-                            <Link href="" underline="none" color="inherit">Products</Link>
-                            <Link href="" underline="none" color="inherit">Travel / Transport</Link>
-                            <Link href="" underline="none" color="inherit">Environment/
+                            <Link href="/sectors/Environment & Farming" underline="none" color="inherit">Environment &
                                 Farming</Link>
-                            <Link href="" underline="none" color="inherit">NGO</Link>
-                            <Link href="" underline="none" color="inherit">Education</Link>
+                            <Link href="/sectors/NGO" underline="none" color="inherit">NGO</Link>
+                            {/*<Link href="" underline="none" color="inherit">Education</Link>*/}
+                            {/*<Link href="" underline="none" color="inherit">Apparel</Link>*/}
+                            {/*<Link href="" underline="none" color="inherit">Travel / Transport</Link>*/}
                         </Container>
                     </Grid>
                     <Grid item xs={12} sm={4} style={{textAlign: 'left'}}>
                         <Container style={{display: 'flex', padding: '20px 0'}}>
                             <LocationOnIcon sx={{color: Color.white, fontSize: 50}}/>
-                            <Typography style={{color: Color.regular, fontSize: 20}}> Black Bridge, Orutota Rd, Gampaha,
-                                Sri
-                                Lanka</Typography>
+                            <Container>
+                                <Typography style={{color: Color.regular, fontSize: 20}}>No: 99/01, Parakandeniya,
+                                    Imbulgoda, Sri Lanka</Typography>
+                            </Container>
                         </Container>
                         <Container style={{display: 'flex', padding: '20px 0'}}>
                             <EmailIcon sx={{color: Color.white, fontSize: 50}}/>
-                            <Typography style={{color: Color.regular, fontSize: 20}}> info@beeinmind.com</Typography>
+                            <Container>
+                                <Typography
+                                    style={{
+                                        color: Color.regular,
+                                        fontSize: 20
+                                    }}> info@imrookaglobalgroup.com</Typography>
+                            </Container>
                         </Container>
                         <Container style={{display: 'flex', padding: '20px 0'}}>
                             <PhoneIcon sx={{color: Color.white, fontSize: 50}}/>
-                            <Typography style={{color: Color.regular, fontSize: 20}}> +94 123 123 123</Typography>
+                            <Container>
+                                <Typography style={{color: Color.regular, fontSize: 20}}> +65 93 52 35 00</Typography>
+                                <Typography style={{color: Color.regular, fontSize: 20}}> +94 773 631 343</Typography>
+                            </Container>
                         </Container>
                     </Grid>
                 </Grid>
